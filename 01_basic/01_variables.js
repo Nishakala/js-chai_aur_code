ea288cbdb28a06f75780418abab21d5776463d01
Nisha_Kala <117833327+Nishakala@users.noreply.github.com>
2024-03-06 10:00:48 +0000

    datatype lecture added

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="nisha@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId=2 not allowed 
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity="Indore"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
