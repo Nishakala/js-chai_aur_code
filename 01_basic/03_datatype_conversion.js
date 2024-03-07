8d2e4dfdca954721b0a1f888ef48d8e6413e012b
Nisha_Kala <117833327+Nishakala@users.noreply.github.com>
2024-03-07 08:37:25 +0000

    datatype converison complete

---
+++ 01_basic/03_datatype_conversion.js
@@ -0,0 +1,34 @@
+let score=""
+//console.log(typeof score);
+let temp=Number (score);
+// console.log(typeof (temp));
+// console.log(temp);
+
+let nullvari=null
+let number=Number(nullvari)
+// console.log(number)
+
+let string="abd"
+let no=String(string)
+//console.log(no)
+
+let islogged=""
+let bool=Boolean(islogged)
+//console.log(bool)
+
+
+//          OPERATIONS
+console.log("2"+1+4)
+console.log(1+2+"4")
+console.log(+true)
+console.log(+"")
+
+let x=5;
+++x
+console.log(x)
+
+
+
+
+
+
